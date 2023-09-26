d504c5079bf5d0d7edcd3a057980920c4ab0c2c9
ajayak5235 <131739918+ajayak5235@users.noreply.github.com>
2023-09-27 02:19:40 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const sequelize = require('./Models/expense');
+const Router = require('./Routes/routes');
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/', Router);
+
+sequelize.sync()
+	.then(res => {
+		console.log(res);
+		app.listen(4000);
+	})
+	.catch(err => console.log(err));
+console.log('Server is Running');
